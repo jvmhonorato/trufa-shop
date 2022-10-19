dd5e17b07d447b3a4f86bcc34afe73b53b6d70d7
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-10-18 22:59:49 -0300

    Build backend PoC

--- poc/pix-auth.js
+++ poc/pix-auth.js
@@ -16,28 +16,28 @@ const getToken = async() => {
     console.log('getToken', process.env.GN_ENV)
     const certificado = fs.readFileSync('../'+process.env.GN_CERTIFICADO)
     const credentials = {
-        client_id: process.GN_CLIENT_ID,
+        client_id: process.env.GN_CLIENT_ID,
         client_secret: process.env.GN_CLIENT_SECRET
     }
-    const data = JSON.stringify({grant_type: 'client_credentials'})
+    const data = JSON.stringify({ grant_type: 'client_credentials' })
     const dataCredentials = credentials.client_id + ':' + credentials.client_secret
     const auth = Buffer.from(dataCredentials).toString('base64')
 
     const agent = new https.Agent({
         pfx: certificado,
-        passphrase: ''
+        passphrase: '',
     })
 
     const config ={
         method: 'POST',
         url: baseURL + '/oauth/token' ,
         headers: {
-            Authorization: 'Basic'+auth,
+            Authorization: 'Basic'+ auth,
             'Content-type': 'application/json'
         },
         httpsAgent: agent,
-        data: data
-    }
+        data: data,
+    } 
     const result = await axios(config)
     console.log(result.data)
 
